bae13469e416fe01058127a06e5ffec2123df954
mkhoatd <binnhatle@gmail.com>
2023-02-27 19:19:40 +0700

    abc

--- script-1.py
+++ script-1.py
@@ -4,6 +4,7 @@
 import os
 import librosa
 import numpy as np
+import matplotlib.pyplot as plt
 
 # speech-silence and voice-unvoiced
 BASE_PATH = None
@@ -20,54 +21,6 @@ def read_lab(lab_file_name: str):
             data.append(line.split())
     return data
 
-
-def get_closest(arr, values):
-    """Get closest value in an sorted array
-    arr: np.ndarray
-    values: List of values to find
-    Return: List of closest values to input values
-    """
-    arr = np.array(arr)
-    values = np.array(values, dtype=np.float64)
-    idx = np.searchsorted(arr, values)
-    idx = np.array(idx)
-    idx[arr[idx] - values > np.diff(arr).mean() * 0.5] -= 1
-    return arr[idx]
-
-
-def get_closest_idx(arr, values):
-    """Get closest index in an sorted array
-    arr: np.ndarray
-    values: List of values to find
-    Return: List of closest index to input values
-    """
-    arr = np.array(arr)
-    values = np.array(values, dtype=np.float64)
-    idx = np.searchsorted(arr, values, side="left")
-    idx = np.array(idx)
-    return idx
-
-
-def array_norm(arr: np.ndarray):
-    min_arr = np.min(arr)
-    max_arr = np.max(arr)
-    return (arr - min_arr) / (max_arr - min_arr)
-
-
-def array_norm_by_threshold(arr: np.ndarray, threshold):
-    """Normalize given function by threshold
-    arr: np.ndarray
-    T: threshold
-    """
-    min_arr = min(arr)
-    max_arr = max(arr)
-    return np.where(
-        arr >= threshold,
-        (arr - threshold) / (max_arr - threshold),
-        (arr - threshold) / (threshold - min_arr),
-    )
-
-
 # %%
 
 
@@ -81,68 +34,6 @@ def load_data(audio_name: str):
     return signal, sr, t, timestamp_label
 
 
-# def separate_frames(signal, sr, frame_length=0.02):
-#     """Separate signal into frames
-#     signal: np.ndarray
-#     sr: sampling rate
-#     frame_length: length of frame
-#     Return: Array of frames
-#     """
-#     frame_size = int(sr * frame_length)
-#     frame_count = len(signal) // frame_size
-#     signal_frames = []
-#     for i in range(frame_count):
-#         startIdx = k*frame_size
-#         stopIdx = startIdx + frame_size
-#         window = np.zeros(signal.shape)
-#         window[]
-#     return np.array(signal_frames), frame_size, frame_count
-
-
-def calc_STE(signal, sr, frame_length=0.02):
-    """Calculate STE
-    signal_frames: Array of frames
-    Return: Array of STE for each frame
-    """
-    STE = []
-    frame_size = int(sr*frame_length)
-    frames_count = len(signal) // frame_size
-    frame_edges = []
-    for i in range(frames_count):
-        startIdx = i*frame_size
-        stop_Idx = startIdx + frame_size
-        window = np.zeros(signal.shape)
-        window[startIdx:stop_Idx] = 1
-        value = np.sum(np.square(signal) * window)
-        STE.append(value)
-        frame_edges.append(startIdx)
-    STE = np.array(STE)
-    STE = STE.reshape(-1)
-    frame_edges = np.array(frame_edges)
-    frame_edges = frame_edges.reshape(-1)
-    return STE, frame_edges
-
-def separate_sp_sl(STE, timestamp_label, t):
-    STE_sp = np.array([])
-    STE_sl = np.array([])
-    for line in timestamp_label:
-        if line[2] == "sp":
-            try:
-                idx1 = int(get_closest_idx(t, line[0]))
-                idx2 = int(get_closest_idx(t, line[1]))
-                STE_sp = np.append(STE_sp, STE[idx1:idx2])
-            except:
-                print(line)
-        if line[2] == "sl":
-            try:
-                idx1 = int(get_closest_idx(t, line[0]))
-                idx2 = int(get_closest_idx(t, line[1]))
-                STE_sl = np.append(STE_sl, STE[idx1:idx2])
-            except:
-                print(line)
-    return np.array(STE_sp), np.array(STE_sl)
-
-
 # %%
 BASE_PATH = "TinHieuHuanLuyen"
 audio_name_list = list(filter(lambda x: x.endswith(".wav"), os.listdir(BASE_PATH)))
@@ -154,35 +45,11 @@ timestamp_label_list = [0] * len(audio_name_list)
 signal_frames_list = [0] * len(audio_name_list)
 frame_size_list = [0] * len(audio_name_list)
 frames_count_list = [0] * len(audio_name_list)
-STE_list = [0] * len(audio_name_list)
-STE_speech_list = [0] * len(audio_name_list)
-STE_silence_list = [0] * len(audio_name_list)
-T_STE_list = [0] * len(audio_name_list)
-# for audio in audio_name_list:
-#     signal_list[i], sr_list[i], t_list[i], timestamp_label_list[i] = load_data(
-#             audio_name_list[i])
-
-
-# %%
 i = 0
 signal_list[i], sr_list[i], t_list[i], timestamp_label_list[i] = load_data(
     audio_name_list[i]
 )
 
-# %%
-signal_list[i], sr_list[i], t_list[i], timestamp_label_list[i]
-
-# %%
-t_list[0][-1]
-
-# %%
-audio_name_list
-import matplotlib.pyplot as plt
-
-# %%
-# %%
-
-# plt.axvline(x = )
 
 # %%
 # def cost(threshold):
@@ -240,8 +107,6 @@ class SpeechSlienceDiscriminator:
             STE_in_silence[idx_pair[0]:idx_pair[1]+1] = 1
         self.STE_in_silence = STE_in_silence
         
-        
-        
     def plot_ste(self):
         plt.plot(self.STE, color = "yellow")
         threshold = self.STE[self.silent_frame_idx[0][1]]
@@ -251,41 +116,15 @@ class SpeechSlienceDiscriminator:
             plt.axvline(x = idx_pair[1], color = "red") 
         plt.show()
         
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
     
-    
-    # def cost(self, threshold):
-    #     self.calc_silent_frame_idx()
-
         
-        # retun ste_bellow_threshold
-        # flipped = ste_bellow_threshold == False
-        # plt.plot(ste_bellow_threshold)
-        # plt.show()
-        # transition_idx = ~flipped & np.roll(flipped, -1)
-        # transition_idx[0] = True
-        # transition_idx[-1] = True
-         
-        # print(transition_idx)
-        
-        # return transition_idx
-        
-    
     def cross_entropy(self, y_hat):
         y = self.STE_in_silence
         loss = -(y * np.log(y_hat) + (1-y) * np.log(1-y_hat))
         return np.sum(loss)
     
-    def derivative_cross_entropy(self, x):
-        t = self.STE_in_silence
-        dLoss = t - x
-        return dLoss
-    
-    def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
-    
-    def derivative_sigmoid(self, x):
-        return self.sigmoid(x) * (1-self.sigmoid(x))
-        
     def logistic_regression(self):
         w = np.random.normal(size = (1, ))
         b = 0
@@ -302,50 +141,7 @@ class SpeechSlienceDiscriminator:
             y_hat = self.sigmoid(z)
             loss = self.cross_entropy(y_hat)
             print(loss)
-            
-        
         return w, b
-        
-        
-        
-        
-        
-        
-    def run(self):
-        thresholds = np.linspace(np.min(self.STE), 0.01, 1000)
-        costs = []
-        for threshold in thresholds:
-            costs.append(self.cost(threshold))
-        costs= np.array(costs)
-        plt.plot(costs)
-        
-        
-        
-        
-        
-
-def calc_STE(signal, sr, frame_length=0.02):
-    """Calculate STE
-    signal_frames: Array of frames
-    Return: Array of STE for each frame
-    """
-    STE = []
-    frame_size = int(sr*frame_length)
-    frames_count = len(signal) // frame_size
-    frame_edges = []
-    for i in range(frames_count):
-        startIdx = i*frame_size
-        stop_Idx = startIdx + frame_size
-        window = np.zeros(signal.shape)
-        window[startIdx:stop_Idx] = 1
-        value = np.sum(np.square(signal) * window)
-        STE.append(value)
-        frame_edges.append(startIdx)
-    STE = np.array(STE)
-    STE = STE.reshape(-1)
-    frame_edges = np.array(frame_edges)
-    frame_edges = frame_edges.reshape(-1)
-    return STE, frame_edges
 # %%
 a = SpeechSlienceDiscriminator(audio_name_list[0], signal_list[0], sr_list[0], t_list[0], timestamp_label_list[0])
 
